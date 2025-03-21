15ca9952086af30e109af89fe53032a5735392a5
Nishant Verma <33548155+vermanishant93@users.noreply.github.com>
2025-03-21 19:38:43 +0530

    Azure DevOps With Terraform

---
+++ README.md
@@ -0,0 +1,1 @@
+# Azure DevOps Simple API
---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test;
+
+public class UnitTest1
+{
+    ValuesController controller = new ValuesController();
+    [Fact]
+    public void GetReturnsMyName()
+    {
+        int id = 1;
+        var returnValue = controller.Get(id);
+        Assert.Equal($"Reponse, Id: {id}", returnValue.Value);
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
